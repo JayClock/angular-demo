cd37253c53a6df14dda8c86dcc915223b25ac147
JayClock <891853602@qq.com>
2021-12-26 19:16:04 +0800

    life-circle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-demo';
+  num = 1;
+  addNum() {
+    this.num++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LifeCircleComponent } from './base';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LifeCircleComponent
   ],
   imports: [
     BrowserModule,
